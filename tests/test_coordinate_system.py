c2bb0f9d276ed3744214b7c627769cb2ad1e7871
Eric Drejza <edrejza@udel.edu>
2023-07-16 00:37:52 -0400

    Compare values instead of instances.

--- tests/test_coordinate_system.py
+++ tests/test_coordinate_system.py
@@ -9,17 +9,17 @@ from src.position import CartesianPosition, SphericalPosition
 class TestCartesianCoordinateSystem(unittest.TestCase):
   
   def test_create_position_cartesian(self):
-    position = CartesianCoordinateSystem.createPosition(0, 0)
-    self.assertIsInstance(position, CartesianPosition,
-      "Did not create an instance of CartesianPosition")
+    position = CartesianCoordinateSystem.createPosition(0, 5)
+    self.assertEquals(CartesianPosition(position).x, 0)
+    self.assertEquals(CartesianPosition(position).y, 5)
   
 
 class TestSphericalCoordinateSystem(unittest.TestCase):
   
   def test_create_position_spherical(self):
-    position = SphericalCoordinateSystem.createPosition(0, 0)
-    self.assertIsInstance(position, SphericalPosition,
-      "Did not create an instance of SphericalPosition")
+    position = SphericalCoordinateSystem.createPosition(5, 0)
+    self.assertEquals(SphericalPosition(position).r, 5)
+    self.assertEquals(SphericalPosition(position).r, 0)
 
 
 if __name__ == '__main__':
