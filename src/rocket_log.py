adf6694ab12e83b138aed69db87e74883f797b37
Eric Drejza <edrejza@udel.edu>
2023-07-16 17:13:40 -0400

    Added additional columns to RocketLog's DataFrame.

--- src/rocket_log.py
+++ src/rocket_log.py
@@ -4,14 +4,24 @@ class RocketLog:
   def __init__(self) -> None:
 
     self.data = pd.DataFrame(columns=[
-      'time',
-      'rocket_component_id',
-      'x',
-      'y',
-      'v',
-      'alpha',
-      'direction',
-      'v_x',
-      'v_y',
-      'fuel_mass'
+      'time', # time
+      'rocket_component_id', # Unique rocket component identifier
+      'x', # x position
+      'y', # y position
+      'v_magnitude', # magnitude of velocity
+      'v_alpha', # angle of velocity
+      'v_direction', # direction of velocity
+      'v_x', # x value of velocity
+      'v_y', # y value of velocity
+      'fuel_mass', # mass of fuel
+      'structure_mass', # mass of structure
+      'drag_force',
+      'gravity_force',
+      'lift_force',
+      'thrust_force',
+      'atmosphere_density',
+      'atmosphere_gravity',
+      'atmosphere_pressure',
+      'atmosphere_temperature',
+      'atmosphere_viscosity'
     ])
