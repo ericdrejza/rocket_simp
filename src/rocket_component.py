78a55f59f2b9cda7657371de3472d9a468f2f0d9
Eric Drejza <edrejza@udel.edu>
2023-07-16 23:52:10 -0400

    Merge rocket_component files.

--- src/rocket_component.py
+++ src/rocket_component.py
@@ -1,4 +1,5 @@
 from abc import ABC
+import math
 
 from atmosphere import Atmosphere
 from position import Position
@@ -9,9 +10,92 @@ from rocket_log import RocketLog
 class RocketComponent(ABC):
   
   def __init__(self, position: Position) -> None:
+    # Basic Properties (Falcon 9 ex.)
+    self.position = position
+    self.length = 70  # m
+    self.width = 4  # m
+    self.velocity = 0  # m/s
+
+    # Mass Properties (Falcon 9 ex.)
+    self.mass_fuel = 375000  # kg
+    self.fuel_flow_rate = 1450  # kg/s
+    self.mass_structure = 25000  # kg
+
+    # Engine Properties
+    self.area_exhaust = math.pi * (self.width / 2) ** 2
+    self.pressure_exhaust = 70927.5  # N/m^2
+    self.velocity_exhaust = 3000  # m/s
+
+    # Forces
+    self.drag_force = self.calc_drag_force()  # N
+    self.gravity_force = self.calc_gravity_force()  # N
+    self.lift_force = self.calc_lift_force()  # N
+    self.thrust_force = self.calc_thrust_force()  # N
+
+    # Atmosphere Object
     self.atmosphere = Atmosphere(self.altitude)
-    self.pos = position
+
+    # Rocket Log
     self.log = RocketLog()
+
+  
+  def calc_mass(self, time_step) -> float:
+    """
+    calculate total mass of the system
+    :return float: mass of the rocket at a point in time
+    """
+
+    mass = self.mass_structure + self.mass_fuel - (self.fuel_flow_rate * time_step)
+    return mass
+
+
+  def calc_drag_force(self) -> float:
+    """
+    calculate the drag force acting on the rocket
+    :return float: drag
+    """
+
+    drag_coefficient = 0.75  # approximation, replace later
+    drag_area = math.pi * (self.width / 2)**2
+    drag_force = drag_coefficient * self.atmosphere.density * drag_area * self.velocity ** 2 / 2
+    return drag_force
+
+
+  def calc_gravity_force(self) -> float:
+    """
+    calculate the gravitational force acting on the rocket
+    :return float: gravity
+    """
+    gravitational_constant = 6.674 * 10**-11  # m^3/kg*s
+    mass_earth = 5.9722 * 10**24  # kg
+    radius_earth = 6.371 * 10**6  # m
+    gravity_force = gravitational_constant * mass_earth * self.calc_mass(1) / (radius_earth + self.altitude**2)
+    return gravity_force
+
+
+  def calc_lift_force(self) -> float:
+    """
+    calculate the lift force acting on the rocket
+    :return float: lift
+    """
+
+    lift_coefficient = 1.5 #approximation, replace later, assume vertical launch
+    lift_area = self.length * self.width
+    lift_force = lift_coefficient * self.atmosphere.density * lift_area * self.velocity ** 2 / 2
+    return lift_force
+
+
+  def calc_thrust_force(self) -> float:
+    """
+    calculate the thrust force acting on the rocket
+    :return float: thrust
+    """
+
+    momentum_thrust = self.fuel_flow_rate * self.velocity_exhaust
+    pressure_thrust = (self.atmosphere.pressure - self.pressure_exhaust) * self.area_exhaust
+    thrust_force = momentum_thrust + pressure_thrust
+
+    return thrust_force
 
 
 
@@ -53,3 +137,10 @@ class RocketComponentDecorator(RocketComponent):
     self.rocket_component = None
 
     return rocket_component
+
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
