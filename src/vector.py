215eb7b5e5d335f744168b760b6468d5fee1d44a
Kyle <kyleodonnell@comcast.net>
2023-07-25 19:35:50 -0400

    Update to Vector class __init__() and construction
    Updated initializer to require keyword arguments of either X and Y or R and Theta.  This will create the vector using Rectangular or Polar coordinates respectively.  Created two helper functions to adjust rectangular attributes or polar attributes as needed.

--- src/vector.py
+++ src/vector.py
@@ -4,29 +4,68 @@ class Vector:
   """
   The vector class will create a vector object from a calculated value. It will
   create magnitude, direction, and component attributes.
+
+  Attributes:
+    x : float
+      value of x in cartesian coordinate system
+    y : float
+      value of x in cartesian coordinate system
+    r: float
+      value of radius in polar coordinate system
+    theta : float
+      value of theta (in radians) in polar coordinate system
   """
 
-  def __init__(self, x, y):
+  def __init__(self, *, x=None, y=None, r=None, theta=None):
     """
-    Initializes a vector from spherical components
-    :param x: x-component
-    :param y: y-component
-    """
-
-    self.x = x
-    self.y = y
-    self.r = math.tan(y/x)
-    self.angle = math.atan(self.y/x)
-
-
-  @classmethod
-  def from_spherical(cls, magnitude: float, angle: float):
-    """
-    Initializes a vector from cartesian components
-    :param float magnitude: magnitude of the vector
-    :param float angle: angle of the vector
+    Initializes a vector from either cartesian or polar coordinate 
+    representation.
+    NOTE: Keyword arguments REQUIRED. Either define a vector with x and y 
+    keywords for cartesian components, or r and theta for polar coordinates. 
+    Initializer will throw a ValueError if both coordinate systems are used.
+    
+    Parameters:
+      x : float
+      value of x in cartesian coordinate system
+      y : float
+        value of x in cartesian coordinate system
+      r: float
+        value of radius in polar coordinate system
+      theta : float
+        value of theta (in radians) in polar coordinate system
     """
 
-    x_component = magnitude * math.cos(angle)
-    y_component = magnitude * math.sin(angle)
-    return Vector(x_component, y_component)
+    if x is not None or y is not None:
+      if r is not None or theta is not None:
+        raise ValueError('Cannot define a Vector by Rectangualr and Polar \
+                         coordinate systems.  Use x and y or r and theta')
+      else:
+        self.x = x
+        self.y = y
+        self.update_polar(x=self.x, y=self.y)
+    elif r is not None and theta is not None:
+      self.r = r
+      self.theta = theta
+      self.update_cartesian(self.r, self.theta)
+    else:
+      raise ValueError('Define a Vector by either x and y or r and theta')
+
+
+  def update_polar(self) -> None:
+    """
+    Creates or updates polar coordinate attributes using cartesian coordinate
+    attributes
+    """
+
+    self.r = math.tan(self.y/self.x)
+    self.theta = math.atan2(self.y/self.x)
+
+
+  def update_cartesian(self) -> None:
+    """
+    Creates or updates cartesian coordinate attributes using polar coordinate
+    attributes
+    """
+
+    self.x = self.r * math.cos(self.theta)
+    self.y = self.r * math.sin(self.theta)
